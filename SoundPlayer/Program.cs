39d00c1c6ab2a07ed82d99a804cd0edf1253ddf2
Opilane TTHK <opilane@tthk.ee>
2021-11-10 12:47:41 +0200

    ConsoleUI

---
+++ SoundPlayer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+using System.Media;
+
+namespace SoundPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                SoundPlayer RandomPlayer = new SoundPlayer("")
+                    RandomPlayer.Load();
+                //RandomPlayer.Play();
+                RandomPlayer.PlayLooping();
+            }
+
+            ForegroundColor = ConsoleColor.White;
+            WriteLine(@"
+                                              ,           ^'^  _
+                                              )               (_) ^'^
+         _/\_                    .---------. ((        ^'^
+         (('>                    )`'`'`'`'`( ||                 ^'^
+    _    /^|                    /`'`'`'`'`'`\||           ^'^
+    =>--/__|m---               /`'`'`'`'`'`'`\|
+         ^^           ,,,,,,, /`'`'`'`'`'`'`'`\      ,
+                     .-------.`|`````````````|`  .   )
+                    / .^. .^. \|  ,^^, ,^^,  |  / \ ((
+                   /  |_| |_|  \  |__| |__|  | /,-,\||
+        _         /_____________\ |')| |  |  |/ |_| \|
+       (')         |  __   __  |  '==' '=='  /_______\     _
+      (' ')        | /  \ /  \ |   _______   |,^, ,^,|    (')
+       \  \        | |--| |--| |  ((--.--))  ||_| |_||   (' ')
+     _  ^^^ _      | |__| |('| |  ||  |  ||  |,-, ,-,|   /  /
+   ,' ',  ,' ',    |           |  ||  |  ||  ||_| |_||   ^^^
+.,,|RIP|,.|RIP|,.,,'==========='==''=='==''=='=======',,....,,,,.,ldb
+== Credits ==
+Music: 'Fun House' by Coyote Hearing from youtube.com/audiolibrary
+Art: Haunted house by ldb from https://www.asciiart.eu/holiday-and-events/halloween
+");
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("\n(Press any key to exit)");
+            ReadKey();
+        }
+    }
+}
